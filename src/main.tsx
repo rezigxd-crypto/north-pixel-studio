a2d55e4eb72d3c7c23f2d5964ace42dca1a3f511
rezigxd-crypto <rezigxd@gmail.com>
2026-04-21 18:26:22 +0100

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { AppProvider } from './lib/context.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <AppProvider>
+    <App />
+  </AppProvider>,
+)
